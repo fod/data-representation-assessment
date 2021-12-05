525b3e1a51754e77e8c68f87c23eb4315ad3e140
Fiachra O' Donoghue <fiachra.m.odonoghue@gmail.com>
2021-12-05 16:19:15 +0000

    Project setup

---
+++ initDB.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE IF NOT EXISTS food;
+USE food;
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE food_items (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  calories INT NOT NULL,
+  protein INT NOT NULL,
+  carbs INT NOT NULL,
+  fat INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE food_log (
+  id INT NOT NULL AUTO_INCREMENT,
+  food_id INT NOT NULL,
+  userid INT NOT NULL,
+  date DATE NOT NULL,
+  quantity INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (food_id) REFERENCES food_items(id),
+  FOREIGN KEY (userid) REFERENCES users(id)
+);
+
+
+
+
+
+
---
+++ settings.py
@@ -0,0 +1,9 @@
+# settings.py
+# Food app settings file
+# Author: Fiachra O' Donoghue
+
+EDAMAM_AUTOCOMPLETE_BASE_URL =  "https://api.edamam.com/auto-complete"
+EDAMAM_APP_ID = "92a29ddd"
+EDAMAM_API_KEY = "ff509ac75eec78e04abeb6b7cae56232"
+
+# curl -X GET --header "Accept: application/json" "https://api.edamam.com/auto-complete?app_id=92a29ddd&app_key=ff509ac75eec78e04abeb6b7cae56232&q=bread&limit=10"
