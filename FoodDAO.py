93948715cb4bf57c630732cd03c90b34e437cf5b
Fiachra O' Donoghue <fiachra.m.odonoghue@gmail.com>
2021-12-14 01:27:12 +0000

    Reboot - cleanup, cruft removal, added connection pooling

--- FoodDAO.py
+++ FoodDAO.py
@@ -1,111 +1,212 @@
-# FoodDAO.py
-# Data Access Object for food_items table
+# foodDAO.py
+# Data access methods for food database
 # Author: Fiachra O' Donoghue
 
+from logging import lastResort
 import mysql.connector
 
 from settings import MYSQL_DB, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_USER
 
 class FoodDAO:
+
     db=""
 
+    # Connect to the database
     def connect_to_db(self):
-        self.db = mysql.connector.connect(
+        db = mysql.connector.connect(
             host=MYSQL_HOST,
             user=MYSQL_USER,
             password=MYSQL_PASSWORD,
-            database=MYSQL_DB
+            database=MYSQL_DB,
+            pool_name='conn_pool',
+            pool_size=10
         )
+        return db
+
+
+    # # Get cursor, reconnecting if necessary
+    # def get_cursor(self, **kwargs):
+    #     if not self.db.is_connected():
+    #         self.connect_to_db()
+    #     return self.db.cursor(**kwargs)
+
+    def get_connection(self):
+        db = mysql.connector.connect(
+            pool_name='conn_pool'
+        )
+        return db
 
     def __init__(self):
-        self.connect_to_db()
+        db = self.connect_to_db()
+        db.close()
 
-    def get_cursor(self):
-        if not self.db.is_connected():
-            self.connect_to_db()
-        return self.db.cursor()
+###### USER MANAGEMENT ######
 
-    def add(self, values):
-        cursor = self.get_cursor()
+    # Return a single user record, seraching by name and, optionally, password
+    def get_user_by_name(self, username, password=None):
+        # buffered = True to prevent 'Unread result found' error
+        db = self.get_connection()
+        cursor = db.cursor(dictionary=True, buffered=True)
+        if password is None:
+            cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+        else:
+            cursor.execute('SELECT * FROM users WHERE username = %s AND password = %s', 
+                          (username, password, ))
+        result = cursor.fetchone()
+        db.close()
+        return result
+
+    # Return a single user record, seraching by email address
+    # Used to check if email is already in use
+    def get_user_by_email(self, email):
+        db = self.get_connection()
+        cursor = db.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
+        result = cursor.fetchone()
+        db.close()
+        return result
+
+    # Create a new user record
+    def add_user(self, username, password, email):
+        db = self.get_connection()
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO users (username, password, email) VALUES (%s, %s, %s)", 
+                      (username, password, email))
+        db.commit()
+        lastRowId = cursor.lastrowid
+        db.close()
+        return lastRowId
+
+###### END OF USER MANAGEMENT ######
+
+
+###### FOOD LOG MANAGEMENT ######
+
+    # Return a single food log record, searching food log id
+    def get_entry_by_id(self, log_id):
+        # The query -- gets food item details from food_items table
+        log_entry_query = """
+            SELECT
+                a.name, b.id, b.food_id, b.user_id,
+                DATE_FORMAT(b.date, '%Y-%m-%d') as date,
+                b.quantity
+            FROM
+                food_items a
+                JOIN food_log b ON a.id = b.food_id
+            WHERE
+                b.id = %s
+            """
+        db = self.get_connection()
+        cursor = db.cursor(dictionary=True)
+        cursor.execute(log_entry_query, (log_id,))
+        result = cursor.fetchone()
+        db.close()
+        return result
+
+    # Return all food log records for a user by id
+    def get_log_by_userid(self, user_id):
+        # The query -- calculates nutrition values
+        # based on quantity of food recorded
+        log_query = """
+            SELECT
+                a.id, a.date, b.name, a.quantity,
+                round((b.calories * (a.quantity / 100)), 0) calories,
+                round((b.protein * (a.quantity / 100)), 0) protein,
+                round((b.carbs * (a.quantity / 100)), 0) carbs,
+                round((b.fat * (a.quantity / 100)), 0) fat
+            FROM
+                food_log a
+                JOIN food_items b ON a.food_id = b.id
+            WHERE
+                a.user_id = %s
+            ORDER BY
+                a.date DESC;
+            """
+        db = self.get_connection()
+        cursor = db.cursor(dictionary=True)
+        cursor.execute(log_query, (user_id,))
+        result = cursor.fetchall()
+        db.close()
+        return result
+
+    # Return all food log records for a user by name
+    def get_log_by_username(self, user_name):
+        userid = self.get_user_by_name(user_name)['id']
+        return self.get_log_by_userid(userid)
+
+    # Add a new food log record
+    def log_food(self, user_id, food_id, quantity, date):
+        db = self.get_connection()
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO food_log (user_id, food_id, quantity, date) VALUES (%s, %s, %s, %s)", 
+                        (user_id, food_id, quantity, date))
+        db.commit()
+        lastRowId = cursor.lastrowid
+        db.close()
+        return lastRowId
+
+    # Update an existing food log record
+    def update_log_entry(self, log_id, food_id, user_id, date, quantity):
+        db = self.get_connection()
+        cursor = db.cursor()
+        cursor.execute("UPDATE food_log SET food_id = %s, user_id = %s, date = %s, quantity = %s WHERE id = %s", 
+                        (food_id, user_id, date, quantity, log_id))
+        db.commit()
+        lastRowId = cursor.lastrowid
+        db.close()
+        return lastRowId
+
+    # Delete a food log record
+    def delete_log_entry(self, log_id):
+        db = self.get_connection()
+        cursor = db.cursor()
+        cursor.execute("DELETE FROM food_log WHERE id = %s", (log_id,))
+        db.commit()
+        lastRowId = cursor.lastrowid
+        db.close()
+        return lastRowId
+
+
+###### END OF FOOD LOG MANAGEMENT ######
+
+
+###### FOOD ITEM MANAGEMENT ######
+
+    # Add a food to the database
+    def add_food_item(self, values):
+        db = self.get_connection()
+        cursor = db.cursor()
         sql="INSERT INTO food_items (name, calories, protein, carbs, fat) VALUES (%s, %s, %s, %s, %s)"
         cursor.execute(sql, values)
-        self.db.commit()
-        cursor.close()
+        db.commit()
+        lastRowId = cursor.lastrowid
+        db.close()
+        return lastRowId
 
-    def getOne(self, id):
-        cursor = self.get_cursor()
-        sql="SELECT * FROM food_items WHERE id = %s"
-        values = (id,)
-        cursor.execute(sql, values)
-        result = cursor.fetchone()
-        cursor.close()
-        return self.convertToDictionary(result)
-
-    def update(self, values):
-        cursor = self.get_cursor()
-        sql="UPDATE food_items SET name = %s, calories = %s, protein = %s, carbs = %s, fat = %s WHERE id = %s"
-        cursor.execute(sql, values)
-        self.db.commit()
-        cursor.close()
-
-    def delete(self, id):
-        cursor = self.get_cursor()
-        sql="DELETE FROM food_items WHERE id = %s"
-        values = (id,)
-        cursor.execute(sql, values)
-        self.db.commit()
-        cursor.close()
-
-    def getAll(self):
-        cursor = self.get_cursor()
-        sql="SELECT * FROM food_items"
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        cursor.close()
-        returnArray = []
-        for result in results:
-            returnArray.append(self.convertToDictionary(result))
-        return returnArray
-
-    def convertToDictionary(self, result):
-        colnames=['id','name','calories','protein','carbs','fat']
-        item = {}
-        if result:
-            for i, colName in enumerate(colnames):
-                value = result[i]
-                item[colName] = value
-        return item 
-
+    # Return all partial matches based on a string
+    # Used for autocomplete
     def autocomplete(self, search):
-        cursor = self.get_cursor()
+        db = self.get_connection()
+        cursor = db.cursor()
         sql="SELECT name FROM food_items WHERE name LIKE %s"
         values = ("%" + search + "%",)
         cursor.execute(sql, values)
         results = cursor.fetchall()
-        cursor.close()
+        db.close()
         returnDict = {'query': search, 'suggestions': [x[0] for x in results]}   
         return returnDict
 
-    def getByName(self, name):
-        cursor = self.get_cursor()
+    # Return a single food item by name, returns food_id
+    # Can be used to check if food item exists
+    def get_food_id(self, name):
+        db = self.get_connection()
+        cursor = db.cursor()
         sql="SELECT id FROM food_items WHERE name = %s"
         values = (name,)
         cursor.execute(sql, values)
         result = cursor.fetchall()
-        cursor.close()
+        db.close()
         if result:
             return result[0][0]
         else:
             return None
-
-    def exists(self, name):
-        cursor = self.get_cursor()
-        sql="SELECT id FROM food_items WHERE name = %s"
-        values = (name,)
-        cursor.execute(sql, values)
-        result = cursor.fetchone()
-        cursor.close()
-        if result:
-            return True
-        else:
-            return False
--- foodlogger.py
+++ foodlogger.py
@@ -2,17 +2,18 @@
 # Flask server
 # Author: Fiachra O' Donoghue
 
-from flask import Flask, url_for, request, redirect, abort, jsonify, render_template, session
 import re
 import requests
+from flask import Flask, url_for, request, redirect, \
+                  abort, jsonify, render_template, session
+
+from settings import EDAMAM_PARSER_BASE_URL, SECRET_KEY, \
+                     EDAMAM_AUTOCOMPLETE_BASE_URL, EDAMAM_APP_ID, \
+                     EDAMAM_API_KEY
+
 from FoodDAO import FoodDAO
-from UsersDAO import UsersDAO
-from settings import EDAMAM_PARSER_BASE_URL, SECRET_KEY, EDAMAM_AUTOCOMPLETE_BASE_URL, EDAMAM_APP_ID, EDAMAM_API_KEY
 
-
-foodDAO = FoodDAO()
-usersDAO = UsersDAO()
-
+## Configure and run the Flask server
 app = Flask(__name__, static_url_path='', static_folder='static')
 app.config.from_object(__name__)
 app.config.from_envvar('APP_CONFIG_FILE', silent=True)
@@ -21,7 +22,28 @@ app.secret_key = SECRET_KEY
 if __name__== '__main__':
     app.run()
 
-###### LOGIN AND REGISTRATION ######
+# Instantiate the Data Access Object
+foodDAO = FoodDAO()
+
+
+### User Management ###
+
+# Logging in and out
+
+def session_login(username, password):
+    account = foodDAO.get_user_by_name(username, password)
+    if account:
+        session['loggedin'] = True
+        session['id'] = account['id']
+        session['username'] = account['username']
+        return True
+    else:
+        session['loggedin'] = False
+        session['username'] = None
+        session['id'] = None
+        return False
+
+
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -29,17 +51,15 @@ def login():
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
-        account = usersDAO.get_user(username, password)
-        if account:
-            session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
+        if session_login(username, password):
             msg = 'Logged in successfully!'
-            food_log = getFoodLog(session['username'])
-            return render_template('food_log.html', msg = msg, food_log=food_log)
+            #food_log = foodDAO.get_log_by_username(session['username'])
+            #return render_template('food_log.html', food_log=food_log, msg=msg)
+            return redirect(url_for('food_log'))
         else:
             msg = 'Incorrect username / password'
-    return render_template('login.html', msg = msg)
+            return render_template('login.html', msg = msg)
+    return render_template('login.html', msg = msg)        
 
 @app.route('/logout')
 def logout():
@@ -48,15 +68,23 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
+
+# Registering a new user
 @app.route('/register', methods =['GET', 'POST'])
 def register():
     msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+    # Check if all details are present
+    if request.method == 'POST' \
+            and 'username' in request.form \
+            and 'password' in request.form \
+            and 'email' in request.form :
+        # Extract details
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        account = usersDAO.get_user(username)
-        account_email = usersDAO.get_user_by_email(email)
+        # Check if username or email are already taken
+        account = foodDAO.get_user_by_name(username)
+        account_email = foodDAO.get_user_by_email(email)
         if account:
             msg = 'Account already exists'
         elif account_email:
@@ -66,25 +94,34 @@ def register():
         elif not re.match(r'[A-Za-z0-9]+', username):
             msg = 'Username may only contain alphanumeric characters'
         elif not username or not password or not email:
-            msg = 'Enter details'
+            msg = 'Please enter all details'
         else:
-            usersDAO.create_user(username, password, email)
-            msg = 'You have successfully registered'
+            # Successful registration
+            foodDAO.add_user(username, password, email)
+            # Now log in new user and send to food logger page
+            if session_login(username, password):
+                msg = 'You have successfully registered'
+                food_log = foodDAO.get_log_by_username(session['username'])
+                return redirect(url_for('food_log'))
     elif request.method == 'POST':
         msg = 'Please enter all details'
     return render_template('register.html', msg = msg)
 
 
-###### END LOGIN AND REGISTRATION ######
+### Food Logging ###
 
-### AUTOCOMPLETE ###
+## Autocomplete
 
+# Autocomplete food names in the food logging form
+# Just autocompletes food names already in the database
 @app.route('/autocomplete_log/food', methods=['GET', 'POST'])
 def autocomplete_log():
     query = request.args.get('query')
     results = foodDAO.autocomplete(query)
     return jsonify(results)
 
+# Autocomplete in the 'Add food to DB' form
+# Autocompletes food names from the Edamam Autocomplete API
 @app.route('/autocomplete_add/food', methods=['GET', 'POST'])
 def autocomplete_add():
     query = request.args.get('query')
@@ -93,10 +130,12 @@ def autocomplete_add():
     returnDict = {'query': query, 'suggestions': results}
     return returnDict
 
+# When a food is selected from the autocomplete list
+# This route fetches the nutrition details from the Edamam Parser API
 @app.route('/autocomplete_add_details/', methods=['GET', 'POST'])
 def autocomplete_add_details():
     query = request.args.get('query')
-    api_url = EDAMAM_PARSER_BASE_URL + '?app_id=' + EDAMAM_APP_ID + '&app_key=' + EDAMAM_API_KEY + '&ingr=' + query + '&nutrition-type=logging'
+    api_url = EDAMAM_PARSER_BASE_URL + '?app_id=' + EDAMAM_APP_ID + '&app_key=' + EDAMAM_API_KEY + '&ingr=' + query + '&nutrition-type=logging'         
     results = requests.get(api_url).json()
     details = results['parsed'][0]['food']
     returnDict = {'name':    details['label'] if 'label' in details else query, 
@@ -105,111 +144,47 @@ def autocomplete_add_details():
                   'fat':      round(details['nutrients']['FAT'], 2) if 'FAT' in details['nutrients'] else 0,
                   'carbs':    round(details['nutrients']['CHOCDF'], 2) if 'CHOCDF' in details['nutrients'] else 0}
     return returnDict
-#{'text': 'dried dill weed', 'parsed': [{'food': {'foodId': 'food_agbfjqyaamtjoca777fmtarkdh6u', 'label': 'Dried Dill Weed', 'nutrients': {'ENERC_KCAL': 253.0, 'PROCNT': 19.96, 'FAT': 4.36, 'CHOCDF': 55.82, 'FIBTG': 13.6}, 'category': 'Generic foods', 'categoryLabel': 'food', 'image': 'https://www.edamam.com/food-img/927/927173abe613e0c9124c406d236e81bd.jpg'}, 'quantity': 1.0, 'measure': {'uri': 'http://www.edamam.com/ontologies/edamam.owl#Measure_serving', 'label': 'Serving', 'weight': 1.0}}], 'hints': [{'food': {'foodId': 'food_agbfjqyaamtjoca777fmtarkdh6u', 'label': 'Dried Dill Weed', 'nutrients': {'ENERC_KCAL': 253.0, 'PROCNT': 19.96, 'FAT': 4.36, 'CHOCDF': 55.82, 'FIBTG': 13.6}, 'category': 'Generic foods', 'categoryLabel': 'food', 'image': 'https://www.edamam.com/food-img/927/927173abe613e0c9124c406d236e81bd.jpg'}, 'measures': [{'uri': 'http://www.edamam.com/ontologies/edamam.owl#Measure_serving', 'label': 'Serving', 'weight': 1.0}, {'uri': 'http://www.edamam.com/ontologies/edamam.owl#Measure_gram', 'label': 'Gram', 'weight': 1.0}, {'uri': 'http://www.edamam.com/ontologies/edamam.ow
 
-### END AUTOCOMPLETE ###
 
-### LOG FOOD ###
+# The main app
+@app.route('/food_log', methods=['GET', 'POST'])
+def food_log():
+    # Only show this page if a user is logged in
+    if session['loggedin']:
+        food_log = foodDAO.get_log_by_username(session['username'])
+        return render_template('food_log.html', food_log=food_log)
+    else:
+        return redirect(url_for('login'))
 
-@app.route('/log_food', methods=['POST'])
-def log_food():
-    bad_log_msg = ''
-    user_id = usersDAO.get_user(session['username'])['id']
-    if all (k in request.form for k in ('food', 'quantity', 'date')):
+# Add an entry to the food log
+@app.route('/food_log/add', methods=['GET', 'POST'])
+def food_log_add():
+    if request.method == 'POST':
+        user_id = session['id']
+        # Extract details
         food = request.form['food']
-        quantity = request.form['quantity']
+        food_id = foodDAO.get_food_id(food)
         date = request.form['date']
-        food_id = foodDAO.getByName(request.form['food'])
-        if food_id:
-            quantity = request.form['quantity']
-            date = request.form['date'] 
-            usersDAO.log_food(user_id, food_id, quantity, date)
-        else:
-            bad_log_msg = 'Food does not exist. Please create it first'
-    else:
-        bad_log_msg = 'Please complete all fields.'
-
-    return redirect(url_for('success'))
-    # render_template('food_log.html' , bad_log_msg = bad_log_msg, food_log = usersDAO.get_food_log(user_id))
-    
-@app.route('/success', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def success():
-    user_id = usersDAO.get_user(session['username'])['id']
-    redirect(url_for('log_food'))
-    return render_template('food_log.html', food_log = usersDAO.get_food_log(user_id))
-
-@app.route('/foods')
-def getAllFoods():
-    return jsonify(foodDAO.getAll())
-
-@app.route('/food/<int:id>')
-def getFood(id):
-    return jsonify(foodDAO.getOne(id))
-
-@app.route('/add_food', methods=['POST'])
-def add_food():
-
-    food = (
-        request.form['food'],
-        request.form['calories'],
-        request.form['protein'],
-        request.form['carbs'],
-        request.form['fat']
-    )
-    foodDAO.add(food)
-    msg = f"request.form['food'] added"
-    food_name = request.form['food']
-    user_id = usersDAO.get_user(session['username'])['id']
-    return render_template('food_log.html' , msg = msg, food_log = usersDAO.get_food_log(user_id), food_name = food_name)
-    
-
-@app.route('/food/<int:food_id>', methods=['PUT'])
-def updateFood(food_id):
-    food_item = foodDAO.getOne(food_id)
-    if 'name' in request.json:
-        food_item['name'] = request.json.get('name', food_item['name'])
-    if 'calories' in request.json:
-        food_item['calories'] = request.json.get('calories', food_item['calories'])
-    if 'fat' in request.json:
-        food_item['fat'] = request.json.get('fat', food_item['fat'])
-    if 'carbs' in request.json:
-        food_item['carbs'] = request.json.get('carbs', food_item['carbs'])
-    if 'protein' in request.json:
-        food_item['protein'] = request.json.get('protein', food_item['protein'])
-    
-    return jsonify(foodDAO.update(food_item))
-
-
-@app.route('/food/<int:food_id>', methods=['DELETE'])
-def deleteFood(food_id):
-    if foodDAO.delete(food_id):
-        return jsonify({'result': True})
-    else:
-        return jsonify({'result': False})
-
-
-
+        quantity = request.form['quantity']
+        # Add to database
+        foodDAO.log_food(user_id, food_id, quantity, date)
+        # Return to food logging page
+        food_log = foodDAO.get_log_by_userid(user_id)
+        return render_template('food_log.html', food_log=food_log)
+        
+# Retrieve a log entry by ID
 @app.route('/log/<int:log_id>', methods=['GET', 'PUT', 'DELETE'])
-def log(log_id):
+def get_log_entry(log_id):
     if request.method == 'GET':
-        return jsonify(usersDAO.get_entry_by_id(log_id))
+        log_entry = foodDAO.get_entry_by_id(log_id)
+        return jsonify(log_entry)
     elif request.method == 'PUT':
-        print(request.form)
-        food_id = foodDAO.getByName(request.form['food'])
-        quantity = request.form['quantity']
+        user_id = session['id']
+        food = request.form['food']
+        food_id = foodDAO.get_food_id(food)
         date = request.form['date']
-        user_id = usersDAO.get_user(session['username'])['id']
-        usersDAO.update_food_log(log_id, food_id, user_id, date, quantity)
-        # return render_template('food_log.html' , msg = 'Entry updated', food_log = usersDAO.get_food_log(user_id))
-        return redirect(url_for('success'))
-    elif request.method == 'DELETE':
-        usersDAO.delete_food_log(log_id)
-        user_id = usersDAO.get_user(session['username'])['id']
-        return redirect(url_for('success'))
-        #return render_template('food_log.html' , msg = 'Entry deleted', food_log = usersDAO.get_food_log(user_id))
+        quantity = request.form['quantity']
+        foodDAO.update_log_entry(log_id, food_id, user_id, date, quantity)
+        return redirect(url_for('food_log'))
 
-def getFoodLog(username):
-    userid = usersDAO.get_user(username)['id']
-    return usersDAO.get_food_log(userid)
 
-    
